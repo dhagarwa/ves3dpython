b9c84336f1bb46efcc351c5304cdde2b63713f26
Dhwanit Agarwal <dhwanit@ices.utexas.edu>
2020-02-20 20:55:45 -0600

    updated matlab

--- matlab/integratePatch.m
+++ matlab/integratePatch.m
@@ -1,7 +1,9 @@
 function val = integratePatch(p, f) %p is patch, f is the function. f should be compactly supported
 %Integration using trapezoidal rule
 %f is a scalar function
+    f0 = f.*(p.pou); %multiplying by partition of unity
     fj = f.*(p.J); %multipying by jacobian
+    
     fjh = fj*p.h_u*p.h_v;
     val = sum(fjh);
 
